427a5eb468ae4da8b9b4a7b9fc19c6854e0f11d4
Andrey Zakharchenko <a_zakharchenko_a@mail.ru>
2022-06-29 00:40:01 +0700

    Initial commit

---
+++ src/Filter.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Filter {
+    protected int treshold;
+
+    public Filter(int treshold) {
+        this.treshold = treshold;
+    }
+
+    public List<Integer> filterOut(List<Integer> source) {
+        Logger logger = Logger.getInstance();
+        logger.log("Запускаем фильтрации");
+        List<Integer> result = new ArrayList<>();
+        for (int f : source){
+            if(f >= treshold) {
+                result.add(f);
+                logger.log("Элемент " + "'" + f + "'" + " подходит");
+            }else {
+                logger.log("Элемент " + "'" + f + "'" + " не подходит");
+            }
+        }
+        logger.log(String.format("Прошло фильтр %s элемента из %s", result.size(), source.size()));
+        return result;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Logger logger = Logger.getInstance();
+        logger.log("Запускаем программу");
+        logger.log("Просим пользователя ввести входные данные для списка ");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите размер списка: ");
+        int size = scanner.nextInt();
+        System.out.println("Введите верхнюю границу для значений: ");
+        int maxBorder = scanner.nextInt();
+
+        logger.log("Создаем и наполняем список");
+
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i <= size; i++){
+            list.add(random.nextInt(maxBorder));
+        }
+        System.out.println("Вот случайный список: " + list);
+
+        logger.log("Просим пользователя ввести входные данные для фильтрации");
+        System.out.println("Введите порог для фильтра: ");
+        Filter filter = new Filter(scanner.nextInt());
+        list = filter.filterOut(list);
+
+        logger.log("Выводим результат на экран");
+        System.out.println("Отфильтрованный список: " + list.toString());
+        logger.log("Завершаем программу");
+
+    }
+}
